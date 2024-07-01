7aaa76a2461ddca71027912f46d57ceb87c75cb7
SoUrAv <aansourav@gmail.com>
2024-07-01 13:57:34 +0600

    middleware route conflict fixded

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,37 @@
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
-import { NextRequest, NextResponse } from 'next/server'
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextRequest, NextResponse } from "next/server";
 
-let defaultLocale = 'en'
-let locales = ['en', 'bn']
+let defaultLocale = "en";
+let locales = ["en", "bn"];
 
 function getLocale(request: NextRequest) {
-  const acceptedLanguage = request.headers.get('accept-language') ?? undefined
-  let headers = { 'accept-language': acceptedLanguage }
-  let languages = new Negotiator({ headers }).languages()
+    const acceptedLanguage =
+        request.headers.get("accept-language") ?? undefined;
+    let headers = { "accept-language": acceptedLanguage };
+    let languages = new Negotiator({ headers }).languages();
 
-  return match(languages, locales, defaultLocale)
+    return match(languages, locales, defaultLocale);
 }
 
 export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-  const pathnameIsMissingLocale = locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+    const pathname = request.nextUrl.pathname;
+    const pathnameIsMissingLocale = locales.every(
+        (locale) =>
+            !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    );
 
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
+    if (pathnameIsMissingLocale) {
+        const locale = getLocale(request);
 
-    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
-  }
+        return NextResponse.redirect(
+            new URL(`/${locale}/${pathname}`, request.url)
+        );
+    }
 }
 
 export const config = {
-  matcher: ['/((?!api|assets|.*\\..*|_next).*)'],
-}
+    matcher: [
+        "/((?!api|assets|privacy-policy|terms-of-service|.*\\..*|_next).*)",
+    ],
+};

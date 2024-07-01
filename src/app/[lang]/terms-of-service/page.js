9a51d9b439cc72cbe08ce44510fec2e2519c3c2f
SoUrAv <aansourav@gmail.com>
2024-07-01 12:22:50 +0600

    Terms of Service Page Fixed

---
+++ src/app/[lang]/terms-of-service/page.js
@@ -0,0 +1,51 @@
+const TermsOfService = () => {
+    return (
+        <div className="bg-gray-100 p-8 mt-24">
+            <h1 className="text-xl font-bold text-gray-700 text-center mb-4">
+                Terms of Service
+            </h1>
+            <div className="max-w-4xl mx-auto text-gray-800 text-justify">
+                <h2 className="text-lg font-semibold">1. Agreement to Terms</h2>
+                <p className="mt-2">
+                    By accessing our app, you agree to be bound by these Terms
+                    of Service and all terms incorporated by reference.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">
+                    2. Account Registration
+                </h2>
+                <p className="mt-2">
+                    You must register for an account to access certain features
+                    of our app. You agree to provide accurate and complete
+                    information and keep your account information updated.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">
+                    3. Privacy Policy
+                </h2>
+                <p className="mt-2">
+                    Your use of our app is also subject to our Privacy Policy,
+                    which covers how we collect, use, share, and store your
+                    personal information.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">
+                    4. Use of the App
+                </h2>
+                <p className="mt-2">
+                    You are responsible for your use of the app and for any
+                    consequences thereof. You may use our app only in compliance
+                    with applicable laws and for legitimate purposes.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">
+                    5. Changes to Terms or Services
+                </h2>
+                <p className="mt-2">
+                    We may modify the Terms at any time. We will notify you of
+                    any changes by revising the date at the top of the Terms. By
+                    continuing to use our app after changes are made, you agree
+                    to be bound by the revised Terms.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default TermsOfService;

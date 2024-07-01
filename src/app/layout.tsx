86de2dc3f3b8cf54ee1bfa8810ea9b1c1991bab7
SoUrAv <aansourav@gmail.com>
2024-07-01 12:43:37 +0600

    Header Footer fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,25 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './[lang]/globals.css'
-import { releaseProductsFromCartAndRestoreStock } from './actions/cart.actions'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./[lang]/globals.css";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'LWS Kart',
-  description: 'E-commerce Application of Learn with Sumit',
-}
+    title: "ShopTop",
+    description: "The Exclusive E-commerce Application for Happy Shopping",
+};
 
 export default async function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body className={poppins.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={poppins.className}>{children}</body>
+        </html>
+    );
 }
